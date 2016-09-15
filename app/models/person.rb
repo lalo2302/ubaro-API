8c199183cf404ef78bbbccbfd3c9263e0bd66349
Eduardo Aguilera <laloao.2302@gmail.com>
2016-09-15 12:31:28 -0500

    Get all the transactions of a person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ActiveRecord::Base
   has_many :transactions_sent, class_name: 'Transaction', foreign_key: 'sender_id'
   has_many :transactions_received, class_name: 'Transaction', foreign_key: 'receiver_id'
+
+  def transactions
+    Transaction.where("sender_id = ? OR receiver_id = ?", self.id, self.id)
+  end
 end
