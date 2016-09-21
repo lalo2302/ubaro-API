24a0a25372d8a51e6809441f027dcc8b2ccc9b94
Eduardo Aguilera <laloao.2302@gmail.com>
2016-09-21 09:44:42 -0500

    Fix routes because of postgres

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get ':controller/:action/:param', :format => :json
-  post 'endpoints/transactions/:id', :format => :json
+  get 'endpoints/authenticate/:param', :format => :json, to: 'endpoints#authenticate'
+  get 'endpoints/transactions/:param', :format => :json, to: 'endpoints#transactions'
+  get 'endpoints/balance/:param', :format => :json, to: 'endpoints#balance'
+  post 'endpoints/transaction/:id', :format => :json, to: 'endpoints#transaction'
 end
