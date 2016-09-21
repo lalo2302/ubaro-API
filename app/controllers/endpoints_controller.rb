fb690897542d7e9e238badab0bc114b2776805aa
Eduardo Aguilera <laloao.2302@gmail.com>
2016-09-20 20:32:04 -0500

    Transactions added

---
+++ app/controllers/endpoints_controller.rb
@@ -0,0 +1,37 @@
+class EndpointsController < ApplicationController
+
+  def authenticate
+    # send as ~/477?pass=123
+    phone = params[:param]
+    password = params[:pass]
+    person = Person.find_by(phone: phone, password: password)
+    respond_to do |format|
+      format.json { render json: person }
+    end
+  end
+
+  def transactions
+    id = params[:id]
+    person = Person.find(id)
+    transactions = person.transactions
+    respond_to do |format|
+      format.json { render json: transactions }
+    end
+  end
+
+  def balance
+  end
+
+  def transaction
+    sender = Person.find params[:param]
+    receiver = Person.find_by :phone => params[:phone] 
+    amount = params[:amount]
+    transaction = Transaction.new
+    transaction.sender = sender
+    transaction.receiver = receiver
+    transaction.amount = amount
+    respond_to do |format|
+      format.json { render json: transaction.make }
+    end
+  end
+end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,12 @@
 class Transaction < ActiveRecord::Base
   belongs_to :sender, class_name: 'Person', foreign_key: 'sender_id'
   belongs_to :receiver, class_name: 'Person', foreign_key: 'receiver_id'
+
+  def make
+    sender = Person.find(self.sender_id)
+    receiver = Person.find(self.receiver_id)
+    sender.balance = sender.balance.to_d - amount
+    receiver.balance = receiver.balance.to_d + amount  
+    return true if receiver.save and sender.save and self.save else false
+  end
 end
