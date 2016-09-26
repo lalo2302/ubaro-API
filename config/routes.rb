985c13341765690a65ea59a7e664baa8a5944288
Eduardo Aguilera <laloao.2302@gmail.com>
2016-09-26 12:46:21 -0500

    get request to transaction for interaction from the browser

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'endpoints/transactions/:param', :format => :json, to: 'endpoints#transactions'
   get 'endpoints/balance/:param', :format => :json, to: 'endpoints#balance'
   put 'endpoints/transaction/:id', to: 'endpoints#transaction'
+  get 'endpoints/transaction/:id', to: 'endpoints#transaction'
 end
